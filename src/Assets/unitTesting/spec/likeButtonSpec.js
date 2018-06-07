167bcdcc7d95f8f07f9289e328e54facf252ca6a
NEELASARASWATHI.V <neelasaraswathi.v@psiog.com>
2018-06-07 17:53:19 +0530

    Like Button

---
+++ src/Assets/unitTesting/spec/likeButtonSpec.js
@@ -0,0 +1,10 @@
+describe("likeButton", function(){
+
+    //testcase 1
+    it("should display corresponding storypoints value",function(){
+        expect(AddClick(2)).toEqual(5);
+        expect(AddClick(3)).toEqual(8); 
+        
+    });
+
+});
---
+++ src/Assets/unitTesting/src/likeTrial.js
@@ -0,0 +1,24 @@
+// var i = 0;
+// var likeButton = function(){
+  
+//     if (i<8) {  
+//       document.getElementById('CountedClicks').innerHTML=AddClick(i);
+
+//     } else {
+//       document.getElementById('CountedClicks').innerHTML = "100";
+
+//     }
+ 
+//     i=i+1;
+
+// }   
+function AddClick(a){
+      
+      console.log('#someButton was clicked');
+
+  var storyPoints = [1,3,5,8,13,20,40,100] ; 
+  
+  return storyPoints[a];
+  
+ }
+ 
